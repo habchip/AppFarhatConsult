6293db4bbe2ee44dc81c58d77b8fa66d0be85a8b
HABCHI <pascal.habchi@sogeti.com>
2017-02-22 09:49:26 +0100

    add ihm

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,12 @@
+import AppFarhatConsult.ihm.MainFrame;
+
 public class Main {
-	public static String fileInputName = "PDDI.xlsx";
-	public static String fileRefName   = "BDDPDI.xlsx";
-	public static String fileOutName   = "ARGI.xlsx";
-	public static String sheetOutName  = "feuille";
-	public static String separator     = ",";
-	public static int lineToParseIn    = 1;
+	// Defines for ihm
+	public final static int windowHeight = 700;
+	public final static int windowWidth  = 1000;
 
 	public static void main(String[] arg) {
-		try {
-			ExcelInReader fileParsor = new ExcelInReader(fileInputName);
-			ExcelWriter excelWriter = new ExcelWriter(fileOutName, sheetOutName);
-			ExcelRefReader excelReader = new ExcelRefReader(fileRefName);
-			String[] keywords = fileParsor.Parse(lineToParseIn, separator);
-			int lineNumber = 2;
-
-			// For each keyword found, write a line
-			for (String keyword : keywords) {
-				String[] excelLine = excelReader.read(keyword);
-				excelWriter.addLine(excelLine, lineNumber);
-				lineNumber++;
-			}
-
-			excelWriter.writeAndClose();
-		} catch (Exception e) {
-			System.out.println(e);
-		}
+		@SuppressWarnings("unused")
+		MainFrame mainFrame = new MainFrame(windowWidth, windowHeight);
 	}
 }
